0f4132a273abe435f0c7cec0d12363f6779e1242
masterUNG <phrombutr@gmail.com>
2016-10-10 15:16:11 +0700

    Open Gallery

--- app/src/main/java/appewtc/masterung/iotfarmer/SignUpActivity.java
+++ app/src/main/java/appewtc/masterung/iotfarmer/SignUpActivity.java
@@ -1,16 +1,77 @@
 package appewtc.masterung.iotfarmer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText, userEditText, passwordEditText;
+    private ImageView imageView;
+    private String nameString, userString, passwordString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Bind Widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        userEditText = (EditText) findViewById(R.id.editText2);
+        passwordEditText = (EditText) findViewById(R.id.editText3);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        //Image Controller
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Choose My Image
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "โปรเลือกรูปภาพ"), 0);
+
+            }   // onClick
+        });
+
     }   // Main Method
 
+    @Override
+    protected void onActivityResult(int requestCode,
+                                    int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if ((requestCode == 0) && (resultCode == RESULT_OK)) {
+
+            Log.d("10octV1", "Choose Success");
+
+        }   // if
+
+    }   // onActivityResult
+
+    public void clickSignUpSign(View view) {
+
+        //Get Value From Edit Text
+        nameString = nameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Space
+        if (nameString.equals("") || userString.equals("") || passwordString.equals("")) {
+            // Have Space
+            MyAlert myAlert = new MyAlert(SignUpActivity.this, R.drawable.doremon48,
+                    "มีช่องว่าง", "กรุณากรอก ทุกช่องด้วย คะ");
+            myAlert.myDialog();
+        }
+
+
+    }   // clickSign
 
 }   // Main Class
